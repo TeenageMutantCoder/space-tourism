ae6fce74dcd9900555238e7d2c8940f7a78f8796
Stevon Wright <stevonisepic@gmail.com>
2022-01-20 14:22:21 -0500

    Added typescript, fonts, layout, and some styling information

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,28 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [`gatsby-plugin-styled-components`],
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-layout`,
+      options: {
+        component: require.resolve(`./src/components/layout.tsx`),
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Bellefair\:400`, `Barlow\:400`, `Barlow Condensed\:400`],
+        display: "swap",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true,
+        jsxPragma: `jsx`,
+        allExtensions: true,
+      },
+    },
+  ],
 };
---
+++ src/styles.ts
@@ -0,0 +1,45 @@
+export default {
+  colors: {
+    dark: "#0B0D17",
+    lightBlue: "#D0D6F9",
+    white: "#FFFFFF",
+  },
+  heading1: {
+    font: "Bellefair",
+    fontSize: "150px",
+  },
+  heading2: {
+    font: "Bellefair",
+    fontSize: "100px",
+  },
+  heading3: {
+    font: "Bellefair",
+    fontSize: "56px",
+  },
+  heading4: {
+    font: "Bellefair",
+    fontSize: "32px",
+  },
+  heading5: {
+    font: "Barlow Condensed",
+    fontSize: "28px",
+    characterSpace: 4.75,
+  },
+  subheading1: {
+    font: "Bellefair",
+    fontSize: "28px",
+  },
+  subheading2: {
+    font: "Barlow Condensed",
+    fontSize: "14px",
+    characterSpace: 2.35,
+  },
+  navText: {
+    font: "Barlow Condensed",
+    fontSize: "16px",
+    characterSpace: 2.7,
+  },
+  bodyText: {
+    font: "Barlow",
+  },
+};
