1d63d198344bb242b04038259ee4b2d3e603930e
Stevon Wright <stevonisepic@gmail.com>
2022-01-20 12:15:58 -0500

    Added necessary components, styled-components

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 function Header() {
   return <div>00 Home 01 Destination 02 Crew 03 Technology</div>;
 }
---
+++ src/components/technology.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Technology() {
+  return <div></div>;
+}
+
+export default Technology;
